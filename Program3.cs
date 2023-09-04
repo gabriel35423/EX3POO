e66ebbe8358848157adc9775ff9195868abde668
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:14:27 -0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Diagonal
+    {
+
+        private double d;
+        private double a;
+    
+
+        public void setd(double l)
+        {
+            d = l;
+
+        }
+        public void seta(double l)
+        {
+            d = l ;
+
+        }
+
+        public double getd()
+        {
+            return d;
+        }
+
+        public double geta()
+        {
+            return a;
+        }
+
+        public void calcular()
+        {
+            a = Math.Pow(d, 2) /2;
+        }
+
+
+    }
+
+}
